c10107d7204ca5c7d1c513cc4eea0458cae4f5f1
Seth Hobson <sethlawrencehobson@gmail.com>
2024-08-27 22:43:22 -0400

    started recipe entitiy

---
+++ src/main/java/com/wfd/recipes/entity/Recipe.java
@@ -0,0 +1,13 @@
+package com.wfd.recipes.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+
+
+@AllArgsConstructor
+@Entity
+@Table(name = "recipes")
+public class Recipe {
+
+}
