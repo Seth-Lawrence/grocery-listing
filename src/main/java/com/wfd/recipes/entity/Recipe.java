7943a3fdce75fc2f3a468b58e515a4ec86c5a5d1
Seth Hobson <sethlawrencehobson@gmail.com>
2024-08-27 23:01:05 -0400

    continuation of writing the recipe entity

--- src/main/java/com/wfd/recipes/entity/Recipe.java
+++ src/main/java/com/wfd/recipes/entity/Recipe.java
@@ -1,7 +1,12 @@
 package com.wfd.recipes.entity;
 
+import io.micrometer.common.lang.NonNull;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 
 
@@ -9,5 +14,27 @@ import lombok.AllArgsConstructor;
 @Entity
 @Table(name = "recipes")
 public class Recipe {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NonNull
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Column(name = "category")
+  private String category;
+
+  @Column(name = "instructions")
+  private String instructions;
+
+  @Column(name = "pictureSrc")
+  private String pictureSrc;
+
+  @Column(name = "tags")
+  private String tags = null;
+
+
 
 }
