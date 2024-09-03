075415482fc3f041313feb3500a2a4fc35621622
Seth Hobson <sethlawrencehobson@gmail.com>
2024-09-02 23:17:11 -0400

    added seperate Ingredient entity

---
+++ src/main/java/com/wfd/recipes/entity/Ingredient.java
@@ -0,0 +1,13 @@
+package com.wfd.recipes.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class Ingredient {
+
+}
