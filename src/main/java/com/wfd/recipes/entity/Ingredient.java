a7e2994bbe6cdcacab06c1b2ceb3d6d692880e45
Seth Hobson <sethlawrencehobson@gmail.com>
2024-09-03 22:14:29 -0400

    added id for ingredient table for future seperation

--- src/main/java/com/wfd/recipes/entity/Ingredient.java
+++ src/main/java/com/wfd/recipes/entity/Ingredient.java
@@ -1,5 +1,9 @@
 package com.wfd.recipes.entity;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -8,6 +12,10 @@ import lombok.Setter;
 @RequiredArgsConstructor
 @Getter
 @Setter
+@Table(name = "ingredients")
 public class Ingredient {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 }
