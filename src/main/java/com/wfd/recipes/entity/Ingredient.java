26962ec9b5030f7c746b4006ff2bc468acc788a1
Seth Hobson <sethlawrencehobson@gmail.com>
2024-09-03 22:15:47 -0400

    added ingredient name in ingredient table

--- src/main/java/com/wfd/recipes/entity/Ingredient.java
+++ src/main/java/com/wfd/recipes/entity/Ingredient.java
@@ -2,6 +2,7 @@ package com.wfd.recipes.entity;
 
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import lombok.Getter;
@@ -18,4 +19,8 @@ public class Ingredient {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+
+  @Column(name = "ingredient", nullable = false)
+  private String ingredient;
 }
