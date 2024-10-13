4961105209bb6b2ee2dca4121856e32c66bc64f8
Seth Hobson <sethlawrencehobson@gmail.com>
2024-10-13 17:43:35 -0400

    added /all and /{id} view functions

--- src/main/java/com/wfd/recipes/web/RecipeController.java
+++ src/main/java/com/wfd/recipes/web/RecipeController.java
@@ -2,9 +2,15 @@ package com.wfd.recipes.web;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+import com.wfd.recipes.entity.Recipe;
 import com.wfd.recipes.service.RecipeService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -13,15 +19,14 @@ public class RecipeController {
   RecipeService recipeService;
 
   @GetMapping("/all")
-  public String getMethodName(@RequestParam String param) {
-      return new String();
+  public ResponseEntity<List<Recipe>> getRecipes() {
+
+    return new ResponseEntity<>(recipeService.getRecipes(), HttpStatus.OK);
   }
 
   @GetMapping("/{id}")
-  public String getMethodName(@RequestParam String param) {
-      return new String();
+  public ResponseEntity<Recipe> getRecipe(@PathVariable Long id) {
+
+    return new ResponseEntity<>(recipeService.getRecipe(id),HttpStatus.OK);
   }
-
-
-
 }
