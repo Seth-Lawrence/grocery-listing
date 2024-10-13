7db58e7ab35e078e91768df1473fe03b4c46a44b
Seth Hobson <sethlawrencehobson@gmail.com>
2024-10-12 23:03:53 -0400

    added methods for getRecipe, getRecipes, and unwrapRecipe for RecipeServiceImpl

--- src/main/java/com/wfd/recipes/service/RecipeServiceImpl.java
+++ src/main/java/com/wfd/recipes/service/RecipeServiceImpl.java
@@ -1,8 +1,34 @@
 package com.wfd.recipes.service;
 
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.List;
+import com.wfd.recipes.entity.Recipe;
+import com.wfd.recipes.repository.RecipeRepository;
 
 @Service
-public class RecipeServiceImpl {
+public class RecipeServiceImpl implements RecipeService {
 
+  private RecipeRepository recipeRepository;
+
+  @Override
+  public Recipe getRecipe(Long id) {
+    Optional<Recipe> recipeOptional = recipeRepository.findById(id);
+    Recipe recipe = unwrapRecipe(recipeOptional, id);
+
+    return recipe;
+  }
+
+  @Override
+  public List<Recipe> getRecipes() {
+   List<Recipe> recipes = (List<Recipe>)recipeRepository.findAll();
+
+   return recipes;
+  }
+
+  static Recipe unwrapRecipe(Optional<Recipe> entity, Long recipeId) {
+    if (entity.isPresent()) return entity.get();
+    else throw new Error("Recipe not found");
+  }
 }
